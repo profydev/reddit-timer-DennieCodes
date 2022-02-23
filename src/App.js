097a1528cd6e53f1ab3db6a2369f6c04d695b6ee
Oculareo <oculareo@gmail.com>
2022-02-22 20:07:45 -0500

    Added Routes, Global Styles, Project Structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import styled from "styled-components";
+import GlobalStyles from "./components/GlobalStyles";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Landing from "./components/Landing";
+import Search from "./components/Search";
+
+const AppContainer = styled.div``;
+const Main = styled.main``;
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <Router>
+      <GlobalStyles />
+      <AppContainer>
+        <Header />
+        <Main>
+          <Routes>
+            <Route exact path="/" element={<Landing />} />
+            <Route exact path="/search" element={<Search />} />
+          </Routes>
+        </Main>
+        <Footer />
+      </AppContainer>
+    </Router>
   );
 }
 
---
+++ src/components/GlobalStyles.jsx
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+body {
+  font-family: "Montserrat", sans-serif;
+}
+
+h1, h2 {
+    font-family: "Bitter", serif;
+}
+
+li {
+  list-style-type: none;
+}
+
+a {
+  text-decoration: none;
+}
+`;
